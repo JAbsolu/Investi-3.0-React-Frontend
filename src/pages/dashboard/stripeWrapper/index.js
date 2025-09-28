bf56b59a65177fcfd7eeda4796f05025712d1041
John <Johnson.absolu@hotmail.com>
2025-09-27 21:14:18 -0400

    fix: Restore loading of Stripe public key from environment variable

--- src/pages/dashboard/stripeWrapper/index.js
+++ src/pages/dashboard/stripeWrapper/index.js
@@ -1,8 +1,7 @@
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
-// const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
-const stripePromise = loadStripe("pk_test_51S2FEnFUQWEqeOOpmidiPPLfeWgDqF6NG07g6QATwrOvtQUBCiLA0RKeY2aF7nVYwxxqN8VkUG0w8I5fIgb9ttak00JNfiyfFT");
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const StripeWrapper = ({children}) => {
   return (
